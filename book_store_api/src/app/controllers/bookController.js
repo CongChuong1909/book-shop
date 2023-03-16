24b984bc900eed0a8065543cd6458a0961353817
CongChuong <92925332+CongChuong1909@users.noreply.github.com>
2023-03-16 13:44:35 +0700

    A

--- book_store_api/src/app/controllers/bookController.js
+++ book_store_api/src/app/controllers/bookController.js
@@ -4,33 +4,22 @@ const AuthorModel = require("../models/authorModel");
 const PublisherModel = require("../models/publisherModel");
 
 const APIfeatures = require("../lib/APIfeatures");
+const bookModel = require("../models/bookModel");
 
 const BookController = {
 
     getAll : async (req, res) => {
         try {
-          if (req.query.type) {
-            req.query.type = await TypeModel.findOne({ slug: req.query.type }).then(
-              (data) => data._id
-            );
-          }
-          if (req.query.author) {
-            req.query.author = await AuthorModel.findOne({
-              slug: req.query.author,
-            }).then((data) => data._id);
-          }
-          const features = new APIfeatures(
-            BookModel.find().populate("idType idAuthor idPublisher"),
-            req.query
-          )
-            .typing()
-            .authoring()
-            .searching();
-          const books = await features.query.then((data) => data);
-    
-          return res.status(200).json(books);
-        } catch (err) {
-          return res.status(500).json({ msg: "getAllBook: " + err.message });
+            await bookModel
+                .find()
+                .populate({ path: "idType" })
+                .populate({ path: "idPublisher" })
+                .populate({ path: "idAuthor" })
+                .then((data) => {
+                    return res.status(200).json(data);
+                });
+        } catch (error) {
+            return res.status(500).json({ msg: "getAllOrder: " + err });
         }
       },
 
